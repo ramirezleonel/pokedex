08196d331172a760b7118ae981479fb979ced0cb
leonel ramirez <leonel.ramirez@grupokonecta.com>
2022-04-21 19:57:29 -0300

    se realizó modificacion para los colores de las card

--- main.js
+++ main.js
@@ -3,9 +3,25 @@ const url_root_pokeAPI = "https://pokeapi.co/api/v2/pokemon/";
 const btnAddPokemon = document.getElementById("btnAddPokemon");
 const pokemonContainer = document.getElementById("pokemonContainer");
 const pokemon = document.getElementsByClassName("column-pokemon");
-const btnPrueba = document.getElementById("btnPrueba");
 
-const handledPrueba = () => {};
+const searchPokemon = (valueInput) => {
+  if (valueInput.length > 0) {
+    return fetch(`${url_root_pokeAPI}${valueInput.toLowerCase()}`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(response.statusText);
+        }
+        return response.json();
+      })
+      .catch((e) => {
+        Swal.showValidationMessage(
+          `Ha ocurrido un error: No se encontró Pokemon `
+        );
+      });
+  } else {
+    return Swal.showValidationMessage(`Ingrese el nombre del pokemon`);
+  }
+};
 
 const handledAddPokemon = () => {
   Swal.fire({
@@ -16,39 +32,24 @@ const handledAddPokemon = () => {
     showLoaderOnConfirm: true,
     showCloseButton: true,
     allowOutsideClick: false,
-    preConfirm: (valueInput) => {
-      return fetch(`${url_root_pokeAPI}${valueInput}`)
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error(response.statusText);
-          }
-          return response.json();
-        })
-        .catch((error) => {
-          Swal.showValidationMessage(
-            `Ha ocurrido un error: No se encontró Pokemon `
-          );
-        });
-    },
+    preConfirm: (valueInput) => searchPokemon(valueInput),
   }).then((result) => {
     if (result.isConfirmed) {
       const { id, name, sprites, types } = result?.value;
-      console.log(types);
-      createPokemon(id, name, sprites?.back_default,types[0].type.name);
+      console.log(result?.value);
+      createPokemon(id, name, sprites?.front_default, types[0]?.type.name);
     }
   });
 };
 
-btnAddPokemon.addEventListener("click", handledAddPokemon);
-btnPrueba.addEventListener("click", handledPrueba);
-
 const createPokemon = (id = "", name = "", img = "", type = "") => {
   const pokemon = document.createElement("div");
+  const { typeSpanish, color } = getColorAndTypePokemon(type);
   pokemon.className = "col-3 pokemon";
-  pokemon.innerHTML = ` <div class="card-pokemon" id=${id}>
+  pokemon.innerHTML = ` <div class="card-pokemon ${color}" id=${id}>
   <img src=${img} alt="pokemon" class="card-img">
   <div class="card-name">${capitalizeFirstLetter(name)}</div>
-  <div class="card-type">${translateNamePokemon(type)}</div>
+  <div class="card-type">${typeSpanish}</div>
   </div>`;
   pokemonContainer.appendChild(pokemon);
 };
@@ -57,13 +58,27 @@ const capitalizeFirstLetter = (string) => {
   return string.charAt(0).toUpperCase() + string.slice(1);
 };
 
-const translateNamePokemon = (pokemon) => {
-  switch (pokemon) {
+const getColorAndTypePokemon = (typePokemon) => {
+  switch (typePokemon) {
     case "electric":
-      return "Electrico";
+      return { typeSpanish: "Electrico", color: "color-electric" };
     case "poison":
-      return "Venenoso";
+      return { typeSpanish: "Venenoso", color: "color-poison" };
+    case "fire":
+      return { typeSpanish: "Fuego", color: "color-fire" };
+    case "water":
+      return { typeSpanish: "Agua", color: "color-water" };
+    case "grass":
+      return { typeSpanish: "Planta", color: "color-grass" };
+    case "normal":
+      return { typeSpanish: "Normal", color: "color-normal" };
+    case "ground":
+      return { typeSpanish: "Tierra", color: "color-ground" };
+    case "bug":
+      return { typeSpanish: "Insecto", color: "color-bug" };
     default:
-      return "Otro";
+      return { typeSpanish: "Otro", color: "color-default" };
   }
 };
+
+btnAddPokemon.addEventListener("click", handledAddPokemon);
